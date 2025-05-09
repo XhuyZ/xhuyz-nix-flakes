51149e69d5d61edb9cf08a48888e8579643bda2e
XhuyZ <huylovengoc35@gmail.com>
2025-05-10 00:04:47 +0700

    complete connecting flakes vs home manager

---
+++ packages/default.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+
+{
+  home.packages = [
+    pkgs.neofetch
+    pkgs.fastfetch
+    pkgs.pfetch
+    pkgs.htop
+    pkgs.btop
+    pkgs.neovim
+    pkgs.git
+    pkgs.vim
+    pkgs.cava
+    pkgs.cmatrix
+    pkgs.fzf
+    pkgs.nushell
+    pkgs.libgcc
+    pkgs.tree
+    pkgs.zig_0_12
+    pkgs.starship
+  ];
+}
+
+
